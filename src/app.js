b738a45ead347ed683da4319abef05a9af30b443
Scott Lingner <slingner@gmail.com>
2020-01-24 13:43:50 -0800

    add comments to appjs and also rearranged middleware pipeline

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
+//Configure create variables written in .env file to the environment
 require('dotenv').config();
+//imports Express 
 const express = require('express');
+//Enable middleware functions
 const morgan = require('morgan');
+//Enable Cross Origin Resource Sharing, simplifies the config of CORS in Express
 const cors = require('cors');
+//does the security hiding for us
 const helmet = require('helmet');
+//imports environment from config settings
 const { NODE_ENV } = require('./config');
 
 const app = express();
 
+// Depends on the condition of the environment
+// Morgan - tiny format for production environment
+// Morgan - common format for development environment
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+  // Mounting our middleware, always put helmet before cors!
 app.use(morgan(morganOption)); 
-app.use(cors());
 app.use(helmet());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
   res.json( { message: 'Hello World' } );
